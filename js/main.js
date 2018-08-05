61151d84c713e0b540a57c96d89c590bfd2b6019
oljuanfe <ojuanferrero@gmail.com>
2018-08-05 11:13:48 +0200

    Create li element with 2 img for each card from api petition

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 'use strict';
 
-console.log('>> Ready :)');
-
 var button = document.querySelector('.start-button');
 var inputRadio = document.querySelectorAll('.radio');
+var cardList = document.querySelector('.cardList');
 
 var jsonResponse;
 var urlForFetch = 'https://raw.githubusercontent.com/Adalab/cards-data/master/4.json';
@@ -12,11 +11,7 @@ function handleInputChange() {
   urlForFetch = 'https://raw.githubusercontent.com/Adalab/cards-data/master/'+ event.target.value +'.json';
 }
 
-function handleClick (){
-  console.log('estoy clickando');
-  event.preventDefault();
-  askForCards();
-}
+
 
 function askForCards (){
   fetch(urlForFetch)
@@ -26,8 +21,47 @@ function askForCards (){
     .then(function(json){
       jsonResponse = json;
       console.log('varjson',jsonResponse);
+      console.log('longitud',jsonResponse.length);
+      createListOfCards();
     });
 }
+
+function createListOfCards() {
+  console.log('longitud',jsonResponse.length);
+  cardList.innerHTML = null;
+  jsonResponse.map(function(card){
+    
+    var imgCardUrl = card.image;
+    var newCardItem = document.createElement('li');
+    var newCardImageFront = document.createElement('img');
+    var newCardImageBack = document.createElement('img');
+
+    newCardItem.setAttribute('class','card-item');
+
+    
+    newCardImageFront.setAttribute('src',imgCardUrl);
+    newCardImageFront.setAttribute('class','card-img-front');
+    newCardImageFront.setAttribute('alt','pokemon');
+
+    
+    newCardImageBack.setAttribute('src','https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB');
+    newCardImageBack.setAttribute('class','card-img-back');
+    newCardImageBack.setAttribute('alt','adalab');
+
+    newCardItem.appendChild(newCardImageFront);
+    newCardItem.appendChild(newCardImageBack);
+
+    return cardList.appendChild(newCardItem);
+  });
+}
+
+function handleClick (){
+  console.log('estoy clickando');
+  event.preventDefault();
+  askForCards();
+  
+}
+
 
 
 button.addEventListener('click', handleClick);
