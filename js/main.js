efb125edd9f5e849345b7e981947af968208cc14
oljuanfe <ojuanferrero@gmail.com>
2018-08-05 19:52:04 +0200

    Get functionality when 2 cards are a match

--- js/main.js
+++ js/main.js
@@ -6,16 +6,40 @@ var cardList = document.querySelector('.cardList');
 
 var jsonResponse;
 var urlForFetch = 'https://raw.githubusercontent.com/Adalab/cards-data/master/4.json';
+var imgClickedPair =[];
 
 function handleInputChange() {
   urlForFetch = 'https://raw.githubusercontent.com/Adalab/cards-data/master/'+ event.target.value +'.json';
 }
 
 function handleCardItemClick(){
+  var cardItem = document.querySelectorAll('.card-item');
   var itemClickedImg = event.currentTarget.children;
-  for (var x=0; x < itemClickedImg.length; x++){
+  var frontImg = event.currentTarget.firstChild;
+  var backImg = event.currentTarget.lastChild;
+  console.log('last',backImg);
+  for (var x = 0; x < itemClickedImg.length; x++){
     itemClickedImg[x].classList.toggle('hidden');
+    
   }
+  if (imgClickedPair.length ===2){
+    imgClickedPair = [];
+  }
+  if (backImg.classList.contains('hidden')){
+    console.log('pair', frontImg.dataset.pair);
+    imgClickedPair.push(frontImg.dataset.pair);
+  }
+  if(imgClickedPair[0]===imgClickedPair[1]){
+    console.log('parejaaaa');
+    for(var z = 0; z < cardItem.length; z++){
+      if (imgClickedPair[0]===cardItem[z].firstChild.dataset.pair){
+        console.log('dobles,dobles');
+        cardItem[z].removeEventListener('click',handleCardItemClick);
+      }
+    }
+  }
+  console.log('array', imgClickedPair);
+  console.log('children',itemClickedImg);
 }
 
 function askForCards (){
@@ -37,6 +61,7 @@ function createListOfCards() {
   jsonResponse.map(function(card){
     
     var imgCardUrl = card.image;
+    var pairNumber = card.pair;
     var newCardItem = document.createElement('li');
     var newCardImageFront = document.createElement('img');
     var newCardImageBack = document.createElement('img');
@@ -45,11 +70,13 @@ function createListOfCards() {
 
     newCardImageFront.setAttribute('src',imgCardUrl);
     newCardImageFront.setAttribute('class','card-img-front');
-    newCardImageFront.classList.add('hidden');
+    newCardImageFront.classList.add('hidden','cardFront');
     newCardImageFront.setAttribute('alt','pokemon');
+    newCardImageFront.setAttribute('data-pair',pairNumber);
     
     newCardImageBack.setAttribute('src','https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB');
     newCardImageBack.setAttribute('class','card-img-back');
+    newCardImageBack.classList.add('cardBack');
     newCardImageBack.setAttribute('alt','adalab');
 
     newCardItem.appendChild(newCardImageFront);
@@ -57,7 +84,6 @@ function createListOfCards() {
 
     return cardList.appendChild(newCardItem);
   });
-  
   var cardItem = document.querySelectorAll('.card-item');
   for (var k = 0; k < cardItem.length; k++){
     cardItem[k].addEventListener('click', handleCardItemClick);
